e4ab553da61a18fb1dd68a7bf2c5c98209548fbf
John Crawley <JohnMichaelCrawley@gmail.com>
2020-06-13 00:02:20 +0100

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by John Crawley on 13/06/2020.
+//  Copyright © 2020 John Crawley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
